b599cecb88f342da7216d2658b123e73ced33b75
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-16 14:41:45 +0000

    - Removed SIGNATURE file. - Updated MANIFEST.SKIP file to ignore folder 'eumm/'. - Updated Copyright year information.

--- lib/Address/PostCode/India.pm
+++ lib/Address/PostCode/India.pm
@@ -1,6 +1,6 @@
 package Address::PostCode::India;
 
-$Address::PostCode::India::VERSION = '0.04';
+$Address::PostCode::India::VERSION = '0.05';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Address::PostCode::India - Interface to the India PinCode.
 
 =head1 VERSION
 
-Version 0.04
+Version 0.05
 
 =cut
 
@@ -149,7 +149,7 @@ L<http://search.cpan.org/dist/Address-PostCode-India/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2014 Mohammad S Anwar.
+Copyright (C) 2014 - 2015 Mohammad S Anwar.
 
 This  program  is  free software; you can redistribute it and/or modify it under
 the  terms  of the the Artistic License (2.0). You may obtain a copy of the full
